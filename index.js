6c2a425e5c5658417e38ec0142b1b7bedb40cbd8
habibur <va.habibur@gmail.com>
2023-04-26 19:47:09 +0600

    costomize

--- index.js
+++ index.js
@@ -5,9 +5,35 @@ const cors = require('cors')
 
 app.use(cors())
 const categories = require('./data/categories.json')
+const news = require('./data/news.json')
 
 app.get('/categories', (req, res) => {
     res.send(categories)
+})
+
+// get all news 
+app.get('/news', (req, res) => {
+    res.send(news)
+})
+
+// get one news by id 
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedNews = news.find(n => n._id === id)
+    res.send(selectedNews)
+})
+
+// get news by category 
+app.get('/categories/:id', (req, res) => {
+    const id = req.params.id;
+    if (id === '0') {
+        res.send(news)
+    }
+    else {
+        const categoryNews = news.filter(n => n.category_id === id)
+        res.send(categoryNews)
+    }
+
 })
 
 app.get('/', (req, res) => {
